5837fe5545a1b4a5775bc046d62aff11bf6ebced
Michael Bikovitsky <biko@bikodbg.com>
2022-11-18 11:18:20 +0200

    Move LFSR to separate file

---
+++ src/lfsr.v
@@ -0,0 +1,67 @@
+/*
+ * Galois LFSR.
+ *
+ * Note: The LFSR won't run if any of the resets are asserted.
+ *
+ * Parameters:
+ *  BITS:               Number of bits for the LFSR state.
+ *  TICKS:              Number of clock ticks between LFSR state transitions.
+ *
+ * Signals:
+ *  clk:                Input clock. Resets are synchronous to it.
+ *
+ *  reset_lfsr_i:       Active-high. Resets the LFSR state to the input
+ *                      from (initial_state_i).
+ *  initial_state_i:    --
+ *
+ *  reset_taps_i:       Active-high. Resets the LFSR's taps to the input from (taps_i).
+ *  taps_i:             --
+ *
+ *  state_o:            Outputs the current LFSR state.
+ */
+module lfsr #(
+    parameter BITS = 8,
+    parameter TICKS = 1
+) (
+    input clk,
+
+    input reset_lfsr_i,
+    input [BITS-1:0] initial_state_i,
+
+    input reset_taps_i,
+    input [BITS-1:0] taps_i,
+
+    output [BITS-1:0] state_o
+);
+
+    reg [BITS-1:0] taps;
+    reg [BITS-1:0] lfsr;
+
+    localparam TICK_BITS = ($clog2(TICKS) == 0) ? 1 : $clog2(TICKS);
+    reg [TICK_BITS-1:0] tick_count;
+
+    assign state_o = lfsr;
+
+    always @(posedge clk) begin
+        if (reset_taps_i) begin
+            taps <= taps_i;
+        end else if (reset_lfsr_i) begin
+            tick_count <= 0;
+            lfsr <= initial_state_i;
+        end else begin
+            if (tick_count == TICKS - 1) begin
+                tick_count <= 0;
+
+                // Advance the LFSR
+                if (lfsr[0]) begin
+                    lfsr <= (lfsr >> 1) ^ taps;
+                end else begin
+                    lfsr <= (lfsr >> 1);
+                end
+            end else begin
+                tick_count <= tick_count + 1;
+            end
+        end
+    end
+
+endmodule
--- src/mbikovitsky_top.v
+++ src/mbikovitsky_top.v
@@ -7,44 +7,28 @@ module mbikovitsky_top #(
 
     localparam LFSR_BITS = 5;
 
-    // Decompose the input wires
     wire clk = io_in[0];
     wire reset_lfsr = io_in[1];
     wire reset_taps = io_in[2];
     wire [LFSR_BITS-1:0] data_in = io_in[3+LFSR_BITS-1:3];
 
-    // Assign the output
     seven_segment seven_segment (
-        .value(lfsr),
+        .value(lfsr_out),
         .segments(io_out)
     );
 
-    reg [LFSR_BITS-1:0] taps;
-    reg [LFSR_BITS-1:0] lfsr;
+    wire [LFSR_BITS-1:0] lfsr_out;
 
-    localparam TICK_BITS = ($clog2(CLOCK_HZ) == 0) ? 1 : $clog2(CLOCK_HZ);
-    reg [TICK_BITS-1:0] tick_count;
+    lfsr #(.BITS(LFSR_BITS), .TICKS(CLOCK_HZ)) lfsr(
+        .clk(clk),
 
-    always @(posedge clk) begin
-        if (reset_taps) begin
-            taps <= data_in;
-        end else if (reset_lfsr) begin
-            tick_count <= 0;
-            lfsr <= data_in;
-        end else begin
-            if (tick_count == CLOCK_HZ - 1) begin
-                tick_count <= 0;
+        .reset_lfsr_i(reset_lfsr),
+        .initial_state_i(data_in),
 
-                // Advance the LFSR
-                if (lfsr[0]) begin
-                    lfsr <= (lfsr >> 1) ^ taps;
-                end else begin
-                    lfsr <= (lfsr >> 1);
-                end
-            end else begin
-                tick_count <= tick_count + 1;
-            end
-        end
-    end
+        .reset_taps_i(reset_taps),
+        .taps_i(data_in),
+
+        .state_o(lfsr_out)
+    );
 
 endmodule
